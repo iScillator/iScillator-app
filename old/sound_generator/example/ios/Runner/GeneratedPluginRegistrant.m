42fb1c0399e7cab0a098a9c167f184e5a9073e3e
anantashakti <anton.v.dodonov@gmail.com>
2021-11-22 17:58:40 +0300

    no comments

---
+++ old/sound_generator/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<sound_generator/SoundGeneratorPlugin.h>)
+#import <sound_generator/SoundGeneratorPlugin.h>
+#else
+@import sound_generator;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SoundGeneratorPlugin registerWithRegistrar:[registry registrarForPlugin:@"SoundGeneratorPlugin"]];
+}
+
+@end
