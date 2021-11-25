7ed41f7d8e31ad98b665476c0c04b64faff6233d
DodonovAV <DodonovAV>
2021-11-25 10:36:40 +0300

    +

--- app/lib/config/targets.dart
+++ app/lib/config/targets.dart
@@ -6,7 +6,7 @@ library my_prj.targets;
 
 final targets=
 {
-    "target": {
+    {
         "Muladhara":285,
         "Svadhisthana":396,
         "Manipura":417,
@@ -18,4 +18,22 @@ final targets=
         "Program Pn":426862,
         "Ptogram Ps":[925370,232500,725470]
     }
-};
+}
+;
+
+
+
+/*
+    "target": [
+        {"Muladhara":285},
+        {"Svadhisthana":396},
+        {"Manipura":417},
+        {"Anahata":528},
+        {"Vishudha":639},
+        {"Adjna":741},
+        {"Sahasrara":852},
+        {"Program Inf":[1946704,1674000]},
+        {"Program Pn":426862},
+        {"Ptogram Ps":[925370,232500,725470]}
+    ]
+*/
