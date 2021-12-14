5152b9cd0f42437c330b3abec2cc8f92e9089c02
Your Name <you@example.com>
2021-12-14 21:25:47 +0300

    +

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,16 +1,3 @@
-// iOS issues resolving:
-// 1. Set AudioKit version in sound_generator.podspecs to: s.dependency 'AudioKit', '~> 4.11.1'
-// 2. Set minimum iOS version in podfil to 13
-// 3. Do the following changes in public init(registrar: FlutterPluginRegistrar) method implementation in SwiftSoundGeneratorPlugin:
-//    ...
-//    let messenger = (registrar as? NSObject)?.value(forKey: "messenger")
-//      if messenger == nil {
-//              return
-//          }
-//    let methodChannel = FlutterMethodChannel(name: "sound_generator", binaryMessenger: messenger as! FlutterBinaryMessenger)
-//    self.onChangeIsPlaying = BetterEventChannel(name: "io.github.mertguner.sound_generator/onChangeIsPlaying", messenger: messenger as! FlutterBinaryMessenger)
-//    self.onOneCycleDataHandler = BetterEventChannel(name: "io.github.mertguner.sound_generator/onOneCycleDataHandler", messenger: messenger as! FlutterBinaryMessenger)
-
 // ignore_for_file: import_of_legacy_library_into_null_safe
 
 import 'dart:async';
