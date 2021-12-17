1aed84f029515bb89a84261eed5d435b1c7598f0
Your Name <you@example.com>
2021-12-17 14:13:38 +0300

    +

--- buils.ios/build_ios.sh
+++
@@ -1,7 +0,0 @@
-export PATH="$PATH:/Users/anton/development/flutter/bin"
-
-cd ..
-cd app
-flutter build ios --no-sound-null-safety
-cd ..
-cd build
--- buils.ios/build_ios_ipa.sh
+++
@@ -1,7 +0,0 @@
-export PATH="$PATH:/Users/anton/development/flutter/bin"
-cd ..
-cd app
-flutter build ipa
-# --no-sound-null-safety
-cd ..
-cd build
