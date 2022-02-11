b88b08ccf3c9c9e9b8963ee37ddb76c302572018
DodonovAV <DodonovAV>
2022-02-11 16:25:43 +0300

    +

--- update.sh
+++ update.sh
@@ -1,9 +1,13 @@
 read -t 3  COMMENT
 
+BUILDNAME=`cat build.name`
+BUILDNUMBER=`cat build.number`
+
+
 git add .
 #git pull --all
 git pull  origin HEAD
 
-git commit -a -m "+ $COMMENT"
+git commit -a -m "$BUILDNAME+$BUILDNUMBER $COMMENT"
 #git push --all
 git push -u origin HEAD
